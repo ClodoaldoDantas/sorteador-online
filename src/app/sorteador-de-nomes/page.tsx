4c434adddf6398df922d7e247746bd6253090215
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-10-21 20:06:07 -0300

    Fazendo uploads de arquivos .txt

--- src/app/sorteador-de-nomes/page.tsx
+++ src/app/sorteador-de-nomes/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { FormEvent, useState } from 'react'
-import { Pointer } from 'lucide-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { Pointer, Upload } from 'lucide-react'
 
 import { Center } from '@/components/Center'
 import { Input } from '@/components/Input'
@@ -21,6 +21,29 @@ export default function SorteadorDeNomes() {
 
   const router = useRouter()
   const setResult = useAppStore((state) => state.setResult)
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files
+
+    if (!fileList) return
+
+    const file = fileList.item(0)
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      const readerResult = reader.result
+
+      if (readerResult && typeof readerResult === 'string') {
+        setField(readerResult)
+      }
+    }
+
+    if (file) {
+      reader.readAsText(file)
+    }
+
+    e.target.value = ''
+  }
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault()
@@ -62,13 +85,30 @@ export default function SorteadorDeNomes() {
             <span>item da lista abaixo:</span>
           </div>
 
-          <div className={styles.group}>
+          <div className={styles.textarea}>
             <Textarea
               placeholder="Escreva aqui os nomes que gostaria de sortear, separados por vírgula."
               required
               value={field}
               onChange={(e) => setField(e.target.value)}
             />
+          </div>
+
+          <div className={styles.upload}>
+            <input
+              type="file"
+              accept="text/plain"
+              id="file-upload"
+              onChange={handleFileChange}
+            />
+
+            <label htmlFor="file-upload">
+              <Upload size={20} />
+              <div>
+                Se preferir, selecione um arquivo <strong>.txt</strong> do seu
+                computador
+              </div>
+            </label>
           </div>
 
           <Button type="submit">
